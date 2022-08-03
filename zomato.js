26d35d735c2063174c748d909708f3bfef4b9f6e
abhishekgarfield <abhishek2759@gmail.com>
2022-08-03 15:04:23 +0530

    check

---
+++ zomato.js
@@ -0,0 +1,59 @@
+            function myfunc()
+            {
+                var el=document.getElementById("option_1");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            function myfunct()
+            {
+                var el=document.getElementById("option_2");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            function myfunctio()
+            {
+                var el=document.getElementById("option_3");
+                if(el.style.display==="none")
+                {
+                    el.style.display="block";
+                }
+                else
+                {
+                    el.style.display="none";
+                }
+            }
+            function myphone(x)
+            {
+                var el=document.getElementById("nav_phon_cont");
+                var th =document.getElementById("phon_nav");
+                
+                if(el.style.display ==="none")
+                {
+                    el.style.display="block";
+                    th.style.backgroundColor="white"
+                    th.style.width="100%";
+                    x.className="fa fa-remove";
+                    th.style.position="fixed";
+                    el.style.top="38px";
+                }
+                else
+                {
+                    el.style.display="none"; 
+                    th.style.backgroundColor="transparent";
+                    x.className="fa fa-bars";
+                    th.style.position="relative";
+                }
+            }
+            
